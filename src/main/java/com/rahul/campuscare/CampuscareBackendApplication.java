ae7206e4a48b4c2686190c005fa36f66a6ed7a9a
Rahul Takale <rahultakale698@gmailcom>
2026-06-05 23:47:09 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/rahul/campuscare/CampuscareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.campuscare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuscareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampuscareBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahul/campuscare/CampuscareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.campuscare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampuscareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
